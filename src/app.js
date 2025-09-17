0a891be77dd701a0655dbe2d26516c6b2e214981
Bhavin-307 <sp1757@srmist.edu.in>
2025-09-17 16:14:16 +0530

    Initialize ESLint And Prettier

--- src/app.js
+++ src/app.js
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.status(200).send("Hello From Bhavin")
-})
+app.get('/', (req, res) => {
+  res.status(200).send('Hello From Bhavin');
+});
 
 export default app;
